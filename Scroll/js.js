8f136789dae6224676f52ea50cfef13ed6a005eb
Lelouch <gledich@tut.by>
2017-04-23 22:23:55 +0300

    Add scroll bar

---
+++ Scroll/js.js
@@ -0,0 +1,72 @@
+(function() {
+const scroll = document.getElementById('scroll');
+const scrollBar = document.getElementById('scroll-bar');
+const content = document.getElementById('scrolling-content');
+const container = document.getElementById('container');
+  
+if(container.offsetHeight < content.offsetHeight) {
+scrollBar.style.display = 'block';
+  
+let currentScroll = 0;
+
+scroll.addEventListener('mousedown', (e) => {
+  const scrollBarTop = scrollBar.getBoundingClientRect().top + scrollY;
+  const mousePosition = e.pageY - scroll.getBoundingClientRect().top + scrollY;
+  
+  function mouseMove(e) {
+    let scrollLength = e.pageY - scrollBarTop - mousePosition;
+    changeScroll(scrollLength);
+  }
+  function mouseUp() {
+    document.removeEventListener('mousemove', mouseMove);
+    scrollBar.removeEventListener('mouseup', mouseUp);
+    scroll.style.transition = '';
+  }
+  
+  document.addEventListener('mousemove', mouseMove);
+  document.addEventListener('mouseup', mouseUp);
+  
+  scroll.style.transition = 'initial';
+  e.preventDefault();
+});
+
+scrollBar.addEventListener('click', (e) => {
+  if (e.target.id != 'scroll') {
+  const scrollBarTop = scrollBar.getBoundingClientRect().top + scrollY;
+  changeScroll(e.pageY - scrollBarTop);
+  }
+});
+
+document.addEventListener('keydown', (e) => {
+  if (e.keyCode === 40) {
+    changeScroll(currentScroll + 20);
+    return false;
+  } else if (e.keyCode === 38) {
+    changeScroll(currentScroll - 20);
+    return false;
+  }
+});
+
+container.addEventListener('wheel', (e) => {
+  changeScroll(currentScroll + ((e.deltaY > 0)? 15 : -15));
+});
+
+function changeScroll(scrollLength) {
+  if (scrollLength < 0) {
+    scrollLength = 0;
+  } else if (scrollLength + scroll.offsetHeight > scrollBar.offsetHeight) {
+    scrollLength = scrollBar.offsetHeight - scroll.offsetHeight;  
+  }
+  currentScroll = scrollLength;
+  scroll.style.transform = `translateY(${scrollLength}px)`;
+  
+  changeContainerScroll(scrollLength);
+}
+  
+function changeContainerScroll(scrollLength) {
+  const maxContainerScroll = container.offsetHeight - content.offsetHeight;
+  const maxScrolling = scrollBar.offsetHeight - scroll.offsetHeight;
+  
+  content.style.transform = `translateY(${scrollLength * (maxContainerScroll/maxScrolling)}px)`;
+}  
+}}());
